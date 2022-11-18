3317dd7cdc773d93c2a3812cb32fcd5b8fa31f23
Eda Kurt <ekrhnn@gmail.com>
2022-11-18 15:56:56 +0300

    atmparaCekme

---
+++ src/main/java/AsalSayi/AsalSayi.java
@@ -0,0 +1,13 @@
+package AsalSayi;
+
+public class AsalSayi {
+    public static void main(String[] args) {
+        int sayi;
+        for (int i = 0; i < 100; i++) {
+            if (i%i==0 && i%1==0){
+
+            }
+
+        }
+    }
+}
---
+++ src/main/java/AsalSayi/readme.md
@@ -0,0 +1,1 @@
+Java ile 1 - 100 arasındaki asal sayıları ekrana yazdıran programı yazınız.
